13455263cf25a1daf263458fcb175d28ebb60d56
Your Name <youremail@example.com>
2025-12-05 19:52:37 +0530

    code merhe

--- App.tsx
+++ App.tsx
@@ -5,18 +5,14 @@
  * @format
  */
 
-import { StatusBar, useColorScheme } from 'react-native';
 import {
   SafeAreaProvider,
 } from 'react-native-safe-area-context';
 import AppNavigator from './navigation/AppNavigator';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppNavigator />
     </SafeAreaProvider>
   );
